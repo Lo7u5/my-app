7adc38394a581b5566657aa331f3b4259da01a7e
Lo7u5 <cyymrak@gmail.com>
2021-02-02 00:00:39 +0500

    progress

--- src/components/Messages/Messages.jsx
+++ src/components/Messages/Messages.jsx
@@ -3,8 +3,19 @@ import s from './Messages.module.css';
 
 const Messages = (props) => {
     return (
-        <div>
-            <span>Dialogs</span>
+        <div className={s.dialogs}>
+            <div className={s.contacts}>
+                <div className={`${s.contact} ${s.active}`}>Sasha</div>
+                <div className={s.contact}>Masha</div>
+                <div className={s.contact}>Dasha</div>
+                <div className={s.contact}>Vasya</div>
+                <div className={s.contact}>Andrey</div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.messageItem}>Hey</div>
+                <div className={s.messageItem}>Howdy</div>
+                <div className={s.messageItem}>Lol</div>
+            </div>
         </div>
     )
 }
