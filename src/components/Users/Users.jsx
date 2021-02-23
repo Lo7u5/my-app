4e18e3012cbf33790e7b28f5bcc9e7b610ac6ae5
Lo7u5 <cyymrak@gmail.com>
2021-02-23 18:19:33 +0500

    переписал Users с функциональной на классовую компоненту

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -2,42 +2,41 @@ import React from 'react';
 import s from './Users.module.css';
 import * as axios from "axios";
 
-const Users = (props) => {
+class Users extends React.Component {
 
-    let getUsers = () => {
-        if (props.users.length === 0) {
-            axios.get("http://localhost:3004/users/")
-                .then(response => {
-                    debugger
-                    props.dispatch.setUsers(response.data)
-                })
-        }
+    constructor(props) {
+        super(props);
+        axios.get("http://localhost:3004/users/")
+            .then(response => {
+                debugger
+                this.props.dispatch.setUsers(response.data)
+            })
     }
 
-    return <div>
-        <button onClick={getUsers}>Get Users</button>
-        {props.users.map(users =>
-            <div key={users.id} className={s.usersContainer}>
-                <div className={s.photoAndButton}>
-                    <img className={s.photoUser} src={users.photoUrl} alt={users.fullName}/>
-                    <div>
-                        {users.followed ? <button onClick={() => {
-                                props.dispatch.unfollow(users.id)
-                            }}>Unfollow</button>
-                            : <button onClick={() => {
-                                props.dispatch.follow(users.id)
-                            }}>Follow</button>}
+    render() {
+        return <div>
+            {this.props.users.map(users =>
+                <div key={users.id} className={s.usersContainer}>
+                    <div className={s.photoAndButton}>
+                        <img className={s.photoUser} src={users.photoUrl} alt={users.fullName}/>
+                        <div>
+                            {users.followed ? <button onClick={() => {
+                                    this.props.dispatch.unfollow(users.id)
+                                }}>Unfollow</button>
+                                : <button onClick={() => {
+                                    this.props.dispatch.follow(users.id)
+                                }}>Follow</button>}
+                        </div>
                     </div>
-                </div>
-                <div className={s.usersInfo}>
-                    <div className={s.name}>{users.fullName}</div>
-                    <div className={s.status}>{users.status}</div>
-                    <div className={s.cityName}>{users.location.cityName}</div>
-                    <div className={s.country}>{users.location.country}</div>
-                </div>
-            </div>)}
-    </div>
-
+                    <div className={s.usersInfo}>
+                        <div className={s.name}>{users.fullName}</div>
+                        <div className={s.status}>{users.status}</div>
+                        <div className={s.cityName}>{users.location.cityName}</div>
+                        <div className={s.country}>{users.location.country}</div>
+                    </div>
+                </div>)}
+        </div>
+    }
 }
 
 export default Users;
